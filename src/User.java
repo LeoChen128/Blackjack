d4373200d15740607d44888e96eb1e9028b28943
LeoChen128 <@gmail>
2024-11-12 11:00:03 -0500

    Revised the deck class so that it doesn't create a new deck each time it is called but rather it stores it inside the array deckOfCards. My next focus would be to return 2 numbers inside a list so that I would be able to use those indices to get the cards inside the deck.

---
+++ src/User.java
@@ -0,0 +1,19 @@
+public class User {
+    public String getInitialCards() {
+
+        int random1 = (int) (Math.random() * 53);
+        int random2 = (int) (Math.random() * 53);
+        //test
+        return (random1 + " " + random2);
+    }
+
+
+//return 2 numbers inside of a list
+
+//    public int checkValue()
+//    {
+//
+//        if ()
+//    }
+
+}
