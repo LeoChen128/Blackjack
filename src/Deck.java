09f8001a7c1efd4a9c4cd2767ca85b8879f72db2
LeoChen128 <@gmail>
2024-11-13 10:58:43 -0500

    Implemented the CardValues class in which calculates the value of the cards in scenarios if the player/dealer gets a card that has a suit of royals or they have an ace in their hand. I also finished up the User class in which their score gets calculated there. Next day I would work on the Dealer Class.

--- src/Deck.java
+++ src/Deck.java
@@ -1,33 +1,38 @@
-import java.util.AbstractList;
 import java.util.List;
 import java.util.ArrayList;
 public class Deck {
 
+    public String drawInitialCards;
     private ArrayList<String> deckOfCards;
 
-    public Deck(){
+    public Deck() {
         List<String> cards = new ArrayList<>();
         String[] suit = {"Diamonds", "Hearts", "Clubs", "Spades"};
-        String[] order = {"Ace", "2", "3","4","5","6","7","8","9","10","Jack","Queen","King"};
+        String[] order = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
         deckOfCards = new ArrayList<String>();
         for (int i = 0; i < order.length; i++) {
             for (int j = 0; j < suit.length; j++) {
                 deckOfCards.add(order[i] + " of " + suit[j]);
             }
         }
+
     }
 
     public ArrayList<String> getDeck() {
         return deckOfCards;
     }
 
-    public void draw(){
-
+    public String draw() {
+        // .size gets the size of the list
+        if (deckOfCards.size() > 0) {
+            return deckOfCards.remove(deckOfCards.size() - 1);
+        }
+        return null;
     }
 
-    public void checkAce(){
-
+    public int getSizeDeck()
+    {
+        return deckOfCards.size();
     }
 }
-
 
--- src/User.java
+++ src/User.java
@@ -1,19 +1,50 @@
-public class User {
-    public String getInitialCards() {
+import java.util.ArrayList;
 
-        int random1 = (int) (Math.random() * 53);
-        int random2 = (int) (Math.random() * 53);
-        //test
-        return (random1 + " " + random2);
+public class User {
+    private ArrayList<String> hand;
+    private int score;
+
+    public User() {
+        this.hand = new ArrayList<>();
+        this.score = 0;
+    }
+    public void drawInitialCards(Deck deck) {
+        for (int i = 0; i < 2; i++) {
+            String card = deck.draw();
+            //in case if card returns to null
+            if(card != null)
+            {
+                hand.add(card);
+                score += CardValues.getValue(card);
+            }
+        }
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public ArrayList<String> getHand() {
+        return hand;
+    }
+
+    public boolean over21(){
+        if (score > 21)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void startOver(){
+        // ".clear" removes all elements of the array "hand"
+        hand.clear();
+    }
+
+    public String toString() {
+        return hand.toString() + "User's score: " + score;
     }
 
 
-//return 2 numbers inside of a list
-
-//    public int checkValue()
-//    {
-//
-//        if ()
-//    }
 
 }
